42baaa12e89b1688c839d025dc8e343ae5d2fc44
José Menezes <jaugustonmn@gmail.com>
2024-01-20 12:27:57 -0300

    created RemoteAuthenticationParams

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -8,7 +8,19 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     httpClient.request(url, method: 'post', body: body);
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({required this.email, required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(email: params.email, password: params.secret);
+
+  Map<String, dynamic> toJson() => {'email': email, 'password': password};
 }
