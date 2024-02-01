22389eaaf9f0635661307e43c7b03f6fc2477e92
José Menezes <jaugustonmn@gmail.com>
2024-02-01 09:16:54 -0300

    moved page initialization to a helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,9 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:for_dev/ui/pages/login_page.dart';
 
 void main() {
-  testWidgets('Should load with correct initial value', (tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     const loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets('Should load with correct initial value', (tester) async {
+    await loadPage(tester);
 
     final emailTextField = tester.widget<TextField>(find.byWidgetPredicate(
         (widget) =>
@@ -26,5 +30,9 @@ void main() {
     // final registerButton = tester.widget<TextButton>(
     //     find.byWidgetPredicate((widget) => widget is TextButton));
     // expect(registerButton.onPressed, isNull);
+  });
+
+  testWidgets('Should call validate with correct values', (tester) async {
+    await loadPage(tester);
   });
 }
